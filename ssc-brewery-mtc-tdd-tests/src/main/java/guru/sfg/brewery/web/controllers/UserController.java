d4ceed86d10dfe8d608689a559ebae99f329378a
springframeworkHamza <kizotomandera@hotmail.com>
2021-06-21 13:46:23 +0200

    2FA verification UI

--- ssc-brewery-mtc-tdd-tests/src/main/java/guru/sfg/brewery/web/controllers/UserController.java
+++ ssc-brewery-mtc-tdd-tests/src/main/java/guru/sfg/brewery/web/controllers/UserController.java
@@ -57,6 +57,24 @@ public class UserController {
         }
     }
 
+    @GetMapping("/verify2fa")
+    public String verify2fa() {
+        return "user/verify2fa";
+    }
+
+    @PostMapping
+    public String verifyPostOf2FA(@RequestParam Integer verifyCode){
+        User user = getUser();
+
+        if(googleAuthenticator.authorizeUser(user.getUsername(), verifyCode)){
+            ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).setGoogle2FaRequired(false);
+
+            return "index";
+        } else {
+            return "user/verify2fa";
+        }
+    }
+
     private User getUser() {
         return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
