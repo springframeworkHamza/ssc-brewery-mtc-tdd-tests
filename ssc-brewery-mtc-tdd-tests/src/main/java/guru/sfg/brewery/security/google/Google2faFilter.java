5e63350f646c3d00882c8fb53d9d25dedb61fadb
springframeworkHamza <kizotomandera@hotmail.com>
2021-06-21 13:56:57 +0200

    ssc 2FA filter

---
+++ ssc-brewery-mtc-tdd-tests/src/main/java/guru/sfg/brewery/security/google/Google2faFilter.java
@@ -0,0 +1,54 @@
+package guru.sfg.brewery.security.google;
+
+
+import guru.sfg.brewery.domain.security.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationTrustResolver;
+import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class Google2faFilter extends GenericFilterBean {
+
+   private final AuthenticationTrustResolver authenticationTrustResolver = new AuthenticationTrustResolverImpl();
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && !authenticationTrustResolver.isAnonymous(authentication)){
+            log.debug("Processing 2FA Filter");
+
+            if(authentication.getPrincipal() != null && authentication.getPrincipal() instanceof User){
+                User user = (User) authentication.getPrincipal();
+
+                if (user.getUseGoogle2fa() && user.getGoogle2FaRequired()) {
+                    log.debug("2FA Required");
+
+                    // to do add failure handler
+                }
+            }
+        }
+
+
+        filterChain.doFilter(request, response);
+    }
+}
--- ssc-brewery-mtc-tdd-tests/src/main/java/guru/sfg/brewery/web/controllers/UserController.java
+++ ssc-brewery-mtc-tdd-tests/src/main/java/guru/sfg/brewery/web/controllers/UserController.java
@@ -47,7 +47,7 @@ public class UserController {
 
         if(googleAuthenticator.authorizeUser(user.getUsername(), verifyCode)){
             User savedUser = userRepository.findById(user.getId()).orElseThrow();
-            savedUser.setUserGoogle2fa(true);
+            savedUser.setUseGoogle2fa(true);
             userRepository.save(savedUser);
 
             return "/index";
