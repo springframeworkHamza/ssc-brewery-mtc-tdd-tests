bcabef04940d60ab89219a966c4f7752aead1d5d
springframeworkHamza <kizotomandera@hotmail.com>
2021-06-21 15:19:09 +0200

    disable ssc testing

--- ssc-brewery-mtc-tdd-tests/src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ ssc-brewery-mtc-tdd-tests/src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -20,9 +20,9 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
  * Created by jt on 6/13/20.
  */
 @RequiredArgsConstructor
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@Configuration
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userDetailsService;
@@ -70,65 +70,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .tokenRepository(persistentTokenRepository)
                         .userDetailsService(userDetailsService);
 
-//                      .rememberMe()
-//                      .key("sfg-key")
-//                      .userDetailsService(userDetailsService);
-
-                //h2 console config
                 http.headers().frameOptions().sameOrigin();
     }
-
-    @Bean
-    PasswordEncoder passwordEncoder(){
-        return SfgPasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-   // @Override
- //   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-       // auth.userDetailsService(this.jpaUserDetailsService).passwordEncoder(passwordEncoder());
-
-//        auth.inMemoryAuthentication()
-//                .withUser("spring")
-//                .password("{bcrypt}$2a$10$7tYAvVL2/KwcQTcQywHIleKueg4ZK7y7d44hKyngjTwHCDlesxdla")
-//                .roles("ADMIN")
-//                .and()
-//                .withUser("user")
-//                .password("{sha256}1296cefceb47413d3fb91ac7586a4625c33937b4d3109f5a4dd96c79c46193a029db713b96006ded")
-//                .roles("USER");
-//
-//        auth.inMemoryAuthentication().withUser("scott").password("{bcrypt15}$2a$15$baOmQtw8UqWZRDQhMFPFj.xhkkWveCTQHe4OBdr8yw8QshejiSbI6").roles("CUSTOMER");
-  //  }
-
-    //    @Override
-//    @Bean
-//    protected UserDetailsService userDetailsService() {
-//        UserDetails admin = User.withDefaultPasswordEncoder()
-//                .username("spring")
-//                .password("guru")
-//                .roles("ADMIN")
-//                .build();
-//
-//        UserDetails user = User.withDefaultPasswordEncoder()
-//                .username("user")
-//                .password("password")
-//                .roles("USER")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(admin, user);
-//    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
